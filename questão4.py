d2f9cf0c12f54eac08c832104208ec3769483243
Shofia637 <andradeanashofia@gmail.com>
2026-07-14 08:35:54 -0300

    Criar questão4.py

---
+++ questão4.py
@@ -0,0 +1,23 @@
+soma = 0
+quantidade = 0
+maior = 0
+
+numero = int(input("Digite um número: "))
+
+while numero >= 0:
+    soma = soma + numero
+    quantidade = quantidade + 1
+
+    if numero > maior:
+        maior = numero
+
+    numero = int(input("Digite outro número: "))
+
+if quantidade > 0:
+    media = soma / quantidade
+
+    print("Soma:", soma)
+    print("Média:", media)
+    print("Maior número:", maior)
+else:
+    print("Nenhum número positivo foi informado.")
